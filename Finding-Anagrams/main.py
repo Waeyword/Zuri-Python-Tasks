1e007ee34022d0891d75bea6065004d124521bb2
Waeyword <walldoyeamafaye@gmail.com>
2022-05-27 19:33:43 +0100

    Update

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -1,11 +1,36 @@
-# Check if two words are anagrams 
+# Check if two words are anagrams
 # Example:
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
 
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
+from unittest import result
 
-    return True
+
+def find_anagram(word, anagram):
+    # [assignment] 
+    
+    #Removes all spaces between words and coverts them to lowercase
+    w = word.replace(" ", "").lower()
+    ana = anagram.replace(" ", "").lower()
+
+    #Checks if both words are the same length
+    if (len(w) == len(ana)):
+
+        #sorts the letters in alphabetical order
+        sorted_1 = sorted(w)
+        sorted_2 = sorted(ana)
+
+        if (sorted_1 == sorted_2):
+            print ("\nTrue")
+            #returns True
+        else:
+            print ("\nFalse")
+            #returns False
+    else:
+        print("Invalid input.Try again")
+        #if both words are not the same length, returns the above
+
+find_anagram("hello", "check")
+find_anagram("below", "elbow")
 
--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -1,16 +1,28 @@
 # Read text from a file, and count the occurence of words in that text
 # Example:
-# count_words("The cake is done. It is a big cake!") 
+# count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+from importlib.resources import contents
+from pathlib import Path
+
+
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    # [assignment] Add your code here
+    w = filename
+
+    with open(w) as f:
+        contents = f.read()
+        print (contents)
+
+    # return "Hello World"
 
 
-def count_words():
-    text = read_file_content("./story.txt")
+# def count_words():
+    #text = read_file_content("./story.txt")
     # [assignment] Add your code here
 
-    return {"as": 10, "would": 20}
+    # return {"as": 10, "would": 20}
+
+
+read_file_content("./story.txt")
