7a2704c7eff95b8e0587f124ef63347668383756
Waeyword <walldoyeamafaye@gmail.com>
2022-05-28 00:29:41 +0100

    Update 1.1

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -3,26 +3,32 @@
 # count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-from importlib.resources import contents
-from pathlib import Path
-
-
 def read_file_content(filename):
-    # [assignment] Add your code here
-    w = filename
-
-    with open(w) as f:
-        contents = f.read()
-        print (contents)
-
-    # return "Hello World"
+    # [assignment]
+    with open(filename) as file:
+        data_file = file.read()
+    return data_file
 
 
-# def count_words():
-    #text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment]
+    spl_text = text.split()
+    textDict = {}
+    for i in spl_text:
+        if i in textDict:
+            textDict[i] += 1
+        else:
+            textDict[i] = 1
+    return textDict
+
+    #w = text.split()
+    #x = input("Word you would like to count in the text file: ")
+    #y = w.count(x)
+    #textDict = dict.fromkeys(w[x], y)
+    # print(y)
 
     # return {"as": 10, "would": 20}
 
 
-read_file_content("./story.txt")
+print(count_words())
